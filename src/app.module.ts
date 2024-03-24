f4cd1391f101207cfe28a08c8ccf8ba1299d9f15
dimbag-alex <a.matiazh@gmail.com>
2024-03-24 20:55:08 +0300

    Added some tampletes for future modifing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { WorkShop } from './workshop/workshop.module';
+
 
 @Module({
-  imports: []
+  imports: [WorkShop]
 })
 export class AppModule {}
+
---
+++ src/workshop/worshop.entity.ts
@@ -0,0 +1,8 @@
+export class WorkShop {
+    id: number;
+    fullname: string;
+    address: string;
+    employers: string[]; // потом будет Employer[]
+    services: string[]; // потом будет Service[]
+    orders: string[]; // потом будет Order[]
+  }
